838004c4595a2fc8922968d9906facbd83ec8cae
CharlyAguettaz <55875854+CharlyAguettaz@users.noreply.github.com>
2022-11-29 14:53:53 +0100

    test 1

--- tests.py
+++ tests.py
@@ -4,21 +4,7 @@ from bowlingCi import easy
 
 class TestEasyMethods(unittest.TestCase):
 
-    # testtest
-    # testtest111
-    # testtest
-    # testtest
-    # testtest
-    # test2
-    # test3
-    # test4
-    # test5
-    # test6# test7
-    # test8
-    # test9# test10
-     # test9# test11
-     # test12
-     # test13
+    # test1
 
     #dernier modif
     
