75948ae16ba3652d3d519e5ff72c1877927b80af
clement <clementsergent@yahoo.fr>
2022-12-30 21:47:17 +0100

    lancer 2 quille

--- tests.py
+++ tests.py
@@ -16,7 +16,16 @@ class TestLancer(unittest.TestCase):
         self.assertGreater(score, 0)
         self.assertLessEqual(score, 10)
 
+    def test_lancer_2(self):
+        # Arrange
+        partie = Partie()
 
+        # Action
+        partie.lancer(nombreDeQuille=2)
+        score = partie.getScore()
+
+        # Assert
+        self.assertEqual(score, 2)
 
 if __name__ == '__main__':
     unittest.main()
